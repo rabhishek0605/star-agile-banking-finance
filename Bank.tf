8af456324b2a43927427cf0666c0f1ef5498495c
Abhishek Ravi <149266515+rabhishek0605@users.noreply.github.com>
2024-03-08 16:26:59 +0530

    Update Bank.tf

--- Bank.tf
+++ Bank.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1f"
+  region = "ap-south-1"
 }
 
 resource "aws_vpc" "example" {
@@ -71,9 +71,9 @@ resource "aws_eip" "proj-eip" {
 }
 
 resource "aws_instance" "test-Server" {
-  ami              = "ami-08ad83fdab5ba8166"
+  ami              = "ami-03bb6d83c60fc5f7c"
   instance_type    = "t2.micro"
-  availability_zone = "us-east-1f"
+  availability_zone = "ap-south-1"
   key_name         = "New-KP"
   
   tags = {
